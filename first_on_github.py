fa6f9935f2d2dc759bae27cdc077ee5ae0135d60
Yunus Chich <u.chichev@gmail.com>
2024-06-26 22:52:23 +0300

    add file

--- first_on_github.py
+++ first_on_github.py
@@ -1,2 +1,5 @@
-print("Hello people!")
-print("Hello people 2!")
+def say_something(number: int, word: str) -> str:
+    word = word.capitalize()
+    return word * number
+
+print(say_something(10, "hello! "))
